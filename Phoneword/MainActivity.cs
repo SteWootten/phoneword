e567ea4366a5c3a975d6b29e715f631928839c66
Ste Wootten <stephen.wootten@onthebeach.co.uk>
2017-02-24 15:33:45 +0000

    Functionality to turn alphanumeric into phone number and call

---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace Phoneword
+{
+	[Activity(Label = "Phone Word", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+			callButton.Enabled = false;
+
+			string translatedNumber = string.Empty;
+
+			translateButton.Click += (object sender, EventArgs e) =>
+			{
+				translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+
+				if (string.IsNullOrWhiteSpace(translatedNumber))
+				{
+					callButton.Text = "Call";
+					callButton.Enabled = false;
+				}
+				else
+				{
+					callButton.Text = "Call " + translatedNumber;
+					callButton.Enabled = true;
+				}
+			};
+
+			callButton.Click += (object sender, EventArgs e) =>
+			{
+				var callDialog = new AlertDialog.Builder(this);
+				callDialog.SetMessage("Call " + translatedNumber + "?");
+				callDialog.SetNeutralButton("Call", delegate {
+					var callIntent = new Intent(Intent.ActionCall);
+					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+					StartActivity(callIntent);
+				});
+
+				callDialog.SetNegativeButton("Cancel", delegate {
+				});
+
+				callDialog.Show();
+			};
+		}
+	}
+}
+
---
+++ Phoneword/PhoneTranslator.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+	public static class PhonewordTranslator
+	{
+		public static string ToNumber(string raw)
+		{
+			if (string.IsNullOrWhiteSpace(raw))
+				return "";
+			else
+				raw = raw.ToUpperInvariant();
+
+			var newNumber = new StringBuilder();
+
+			foreach (var character in raw)
+			{
+				if (" -0123456789".Contains(character))
+					newNumber.Append(character);
+				else
+				{
+					var result = TranslateToNumber(character);
+					if (result != null)
+						newNumber.Append(result);
+				}
+				//otherwise we've skipped a non-numeric char
+			}
+
+			return newNumber.ToString();
+		}
+
+		static bool Contains(this string keyString, char character)
+		{
+			return keyString.IndexOf(character) >= 0;
+		}
+
+		static int? TranslateToNumber(char character)
+		{
+			if ("ABC".Contains(character))
+				return 2;
+			else if ("DEF".Contains(character))
+				return 3;
+			else if ("GHI".Contains(character))
+				return 4;
+			else if ("JKL".Contains(character))
+				return 5;
+			else if ("MNO".Contains(character))
+				return 6;
+			else if ("PQRS".Contains(character))
+				return 7;
+			else if ("TUV".Contains(character))
+				return 8;
+			else if ("WXYZ".Contains(character))
+				return 9;
+			return null;
+		}
+}
+}
